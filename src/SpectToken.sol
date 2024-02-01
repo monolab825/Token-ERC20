fba490975edc84b38b864d1dd29f2ebb0d589e8a
monolab825 <p.smiles777@gmail.com>
2024-02-01 20:58:51 +0530

    transfer functionality added

--- src/SpectToken.sol
+++ src/SpectToken.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.18;
 
 contract SpectToken{
 
+    error notEnoughBalance(uint256 senderBalance);
+
     uint256 private s_totalTokens;
     uint8 private s_decimals;
+
+    mapping(address => uint256) private s_balance;
 
     constructor(uint256 totalTokens, uint8 totalDecimals){
         s_totalTokens=totalTokens;
@@ -28,11 +32,26 @@ contract SpectToken{
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance){
-
+        return s_balance[_owner];
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success){
 
+        if(balanceOf(msg.sender) < _value){
+            revert notEnoughBalance(
+                balanceOf(msg.sender)
+            );
+        }
+        
+        uint256 senderBalance = balanceOf(msg.sender);
+        uint256 recieverBalance = balanceOf(_to);
+        
+        s_balance[msg.sender] -= _value;
+        s_balance[_to] += _value;
+
+        if(senderBalance == (balanceOf(msg.sender) - _value) && recieverBalance == (balanceOf(_to) + _value)){
+            return true;
+        }
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
